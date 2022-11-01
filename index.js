6ad8276f78533890edb3c37aa5718e15e4050fc5
kingsleycj <primekings.kc@gmail.com>
2022-11-01 14:31:57 +0100

    jquery event

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
 $('li').on('click', function() {
-    $(this).text('clicked!');
+    $(this).text('clicked!').delay(800).fadeOut(400);
+});
+*/
+
+console.log($('a').attr('href'));
+
+$('a').not('[href *= "my-domain.com"]').attr("target", "_blank");
+
+$('button').on('click', function(){
+    $('a').attr('href', 'https://www.github.com');
+    console.log($('a').attr('href'));
 });
