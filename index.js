b0458fadb74e15a605022a031da51f9a9a3059d8
kingsleycj <primekings.kc@gmail.com>
2022-11-01 14:55:23 +0100

    using on() method

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-/*
+
 $('li').on('click', function() {
-    $(this).text('clicked!').delay(800).fadeOut(400);
+    $(this).text('Clicked!');
 });
-*/
 
-console.log($('a').attr('href'));
 
-$('a').not('[href *= "my-domain.com"]').attr("target", "_blank");
+// console.log($('a').attr('href'));
 
-$('button').on('click', function(){
-    $('a').attr('href', 'https://www.github.com');
-    console.log($('a').attr('href'));
-});
+// $('a').not('[href *= "my-domain.com"]').attr("target", "_blank");
+
+// $('button').on('click', function(){
+//     $('a').attr('href', 'https://www.github.com');
+//     console.log($('a').attr('href'));
+// });
