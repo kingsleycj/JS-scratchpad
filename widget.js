91d099b8f66faaff5eafd86901cedd2d02ab8288
kingsleycj <primekings.kc@gmail.com>
2022-11-12 20:23:42 +0100

    processing JSON data

--- widget.js
+++ widget.js
@@ -2,8 +2,18 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
     var employees = JSON.parse(xhr.responseText);
-    console.log(typeof employees);
+    var statusHTML = '<ul class = "bulleted">';
+    for (let i =0; i < employees.length; i++) {
+        if (employees[i].in-office === true) {
+          statusHTML += '<li class = "in">';
+        } else {
+          statusHTML += '<li class ="out">';
+        }
+        statusHTML += employees[i].name;
+        statusHTML += '</li>';
+      }
     }
   };
 xhr.open("GET", "data.json");
 xhr.send();
+
