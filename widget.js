80e5577496b13bb05b91288461e815bb144e3ca3
kingsleycj <primekings.kc@gmail.com>
2022-11-12 18:20:20 +0100

    parsing JSON data

---
+++ widget.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    var employees = JSON.parse(xhr.responseText);
+    console.log(typeof employees);
+    }
+  };
+xhr.open("GET", "data.json");
+xhr.send();
