0437849757dce316a8f481f12c00f96b4fc94c2f
kingsleycj <primekings.kc@gmail.com>
2022-11-14 22:20:01 +0100

    AJAX and API's

---
+++ flickr.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('button').click(function (){
+        $("button").removeClass("selected");
+        $(this).addClass("selected");
+        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        var animals = $(this).text();
+        var flickerOptions = {
+            tags: animals,
+            format: "json"
+        };
+        function displayPhotos (data) {
+            
+        }
+        $.getJSON(flickrAPI,flickerOptions, displayPhotos);
+    });
+}); //end ready
