96eed29fd81ed6d00d9b3636db4255f91513bf90
kingsleycj <primekings.kc@gmail.com>
2023-01-02 21:16:12 +0100

    some mods

--- demo.js
+++ demo.js
@@ -1,10 +1,12 @@
-var url = 'employee.php';
-var data = {
-    firstname: "Kingsley",
-    lastname : "Nweke"
-};
-var callback = function (response) {
-    // do something with the response
-};
-$.get(url, data, callback);
+// var url = 'employee.php';
+// var data = {
+//     firstname: "Kingsley",
+//     lastname : "Nweke"
+// };
+// var callback = function (response) {
+//     // do something with the response
+// };
+// $.get(url, data, callback);
+
+
 
