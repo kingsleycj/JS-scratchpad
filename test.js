e757b181ba3c9185b68d44f6b3fba2738a4d555c
kingsleycj <primekings.kc@gmail.com>
2023-01-11 03:29:39 +0100

    testing patterns in regEx

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-let res1 = new RegExp("abc");
-let res2 = /abc/.test("abc");
+// let res1 = new RegExp("abc");
+// let res2 = /abc/.test("abc");
 
 // console.log(res2);
 
 // class 2:
 // a test for a pattern that starts with a digit
-let res3 = /\d./.test('abcdef')
-console.log(res3);
+// let res3 = /\d./.test('abcdef')
+// console.log(res3);
