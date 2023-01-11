cb6ba458c3b3ee0a685511046796dc5d87027cd1
kingsleycj <primekings.kc@gmail.com>
2023-01-11 11:59:17 +0100

    testing patterns in regEx

--- test.js
+++ test.js
@@ -7,5 +7,5 @@
 // a test for a pattern that starts with a digit
 // let res3 = /\d./.test('abcdef')
 // console.log(res3);
-const set1 = /\d{3}/.test("323");
+const set1 = /\d{5, 8}/.test("323"); // looks out for a pattern that has a minimum of 5 and maximum of 8 digits in the quoted braces
 console.log(set1)
