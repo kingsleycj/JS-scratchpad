06e30fc7a0a91cf40e163d45332fbc45058318ec
kingsleycj <primekings.kc@gmail.com>
2023-01-12 00:29:49 +0100

    repeating patterns in RegEx

--- test.js
+++ test.js
@@ -7,5 +7,13 @@
 // a test for a pattern that starts with a digit
 // let res3 = /\d./.test('abcdef')
 // console.log(res3);
-const set1 = /\d{5, 8}/.test("323"); // looks out for a pattern that has a minimum of 5 and maximum of 8 digits in the quoted braces
-console.log(set1)
+
+// quantifiers +,  *,  .
+const set1 = /\d[5, 8]/.test("1234");    // looks out for a pattern that has a minimum of 5 and maximum of 8 digits in the quoted braces
+// console.log(set1)
+
+// REPEATING PATTERNS
+// + quantifiers
+const set2 = /hello[0-9]+/.test("hello1") // looks for "hello", "0-9" and "+" represents a repetitive pattern, meaning these patterns can occur more than once
+console.log(set2)
+
