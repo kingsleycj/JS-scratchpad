412dd4c3c52daeb9e8570bb3220e13379a147df0
kingsleycj <primekings.kc@gmail.com>
2023-01-12 03:25:52 +0100

    matches and groups in regEx

--- test.js
+++ test.js
@@ -15,10 +15,14 @@ const set1 = /\d[5, 8]/.test("1234");    // looks out for a pattern that has a m
 // REPEATING PATTERNS
 // + quantifiers
 const set2 = /^\d\S+@\S+\.?\S+$/.test("1king@gmail.com") // looks for a string literal, then "@", then another string literal, then a dot "."(optional), then another string literal
-// console.log(set2)
+console.log(set2)
 
 // A STANDARD EMAIL REGEX VALIDATION
 
 const emailValidation =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.exec("primekings.kc@gmail.com") ;
     console.log(emailValidation)
+
+// MATCHES AND GROUPS
+const match = /\d+/.exec("one two 100")
+console.log(match)
